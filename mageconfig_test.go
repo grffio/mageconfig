572567f847d5af187c0f21cdb44eaae96c1c6afe
Alexander S <pub+github@grffio.com>
2023-08-08 07:30:06 +0300

    Added 'depends' tag

--- mageconfig_test.go
+++ mageconfig_test.go
@@ -2,6 +2,7 @@ package mageconfig
 
 import (
 	"errors"
+	"fmt"
 	"os"
 	"testing"
 
@@ -9,13 +10,15 @@ import (
 )
 
 type TestConfig struct {
-	Field0 bool   `arg:"field0" default:"false"`
-	Field1 string `arg:"field1" default:"default1"`
-	Field2 int    `arg:"field2" default:"2"`
-	Field3 string `env:"FIELD3" default:"envDefault"`
-	Field4 string `file:"field4" default:"fileDefault"`
-	Field5 string `file:"field5" arg:"field5" env:"FIELD5" default:"allDefault"`
-	Field6 string `file:"field6" arg:"field6" env:"FIELD6" required:"true"`
+	DField0 bool   `arg:"dfield0"`
+	DField1 bool   `arg:"dfield1" depends:"Field1,DField0"`
+	Field0  bool   `arg:"field0" default:"false"`
+	Field1  string `arg:"field1" default:"default1"`
+	Field2  int    `arg:"field2" default:"2"`
+	Field3  string `env:"FIELD3" default:"envDefault"`
+	Field4  string `file:"field4" default:"fileDefault"`
+	Field5  string `file:"field5" arg:"field5" env:"FIELD5" default:"allDefault"`
+	Field6  string `file:"field6" arg:"field6" env:"FIELD6" required:"true"`
 }
 
 func TestLoad(t *testing.T) {
@@ -31,29 +34,32 @@ func TestLoad(t *testing.T) {
 			name: "Load with default values",
 			file: "",
 			env:  map[string]string{},
-			args: []string{},
+			args: []string{"-dfield0=true", "-field6=required"},
 			wantConfig: TestConfig{
-				Field1: "default1",
-				Field2: 2,
-				Field3: "envDefault",
-				Field4: "fileDefault",
-				Field5: "allDefault",
+				DField0: true,
+				Field1:  "default1",
+				Field2:  2,
+				Field3:  "envDefault",
+				Field4:  "fileDefault",
+				Field5:  "allDefault",
+				Field6:  "required",
 			},
-			wantErr: errors.New("required parameter not set: Field6"),
+			wantErr: nil,
 		},
 		{
 			name: "Load with args values",
 			file: "",
 			env:  map[string]string{},
-			args: []string{"-field1=arg1", "-field2", "3", "--field0", "-field6=required"},
+			args: []string{"-dfield0=true", "-field1=arg1", "-field2", "3", "--field0", "-field6=required"},
 			wantConfig: TestConfig{
-				Field0: true,
-				Field1: "arg1",
-				Field2: 3,
-				Field3: "envDefault",
-				Field4: "fileDefault",
-				Field5: "allDefault",
-				Field6: "required",
+				DField0: true,
+				Field0:  true,
+				Field1:  "arg1",
+				Field2:  3,
+				Field3:  "envDefault",
+				Field4:  "fileDefault",
+				Field5:  "allDefault",
+				Field6:  "required",
 			},
 			wantErr: nil,
 		},
@@ -65,14 +71,15 @@ func TestLoad(t *testing.T) {
 				"FIELD5": "env5",
 				"FIELD6": "required",
 			},
-			args: []string{},
+			args: []string{"-dfield0=true"},
 			wantConfig: TestConfig{
-				Field1: "default1",
-				Field2: 2,
-				Field3: "env3",
-				Field4: "fileDefault",
-				Field5: "env5",
-				Field6: "required",
+				DField0: true,
+				Field1:  "default1",
+				Field2:  2,
+				Field3:  "env3",
+				Field4:  "fileDefault",
+				Field5:  "env5",
+				Field6:  "required",
 			},
 			wantErr: nil,
 		},
@@ -80,14 +87,15 @@ func TestLoad(t *testing.T) {
 			name: "Load with file values",
 			file: "testdata/config.file",
 			env:  map[string]string{},
-			args: []string{},
+			args: []string{"-dfield0=true"},
 			wantConfig: TestConfig{
-				Field1: "default1",
-				Field2: 2,
-				Field3: "envDefault",
-				Field4: "file4",
-				Field5: "file5",
-				Field6: "file6",
+				DField0: true,
+				Field1:  "default1",
+				Field2:  2,
+				Field3:  "envDefault",
+				Field4:  "file4",
+				Field5:  "file5",
+				Field6:  "file6",
 			},
 			wantErr: nil,
 		},
@@ -98,15 +106,16 @@ func TestLoad(t *testing.T) {
 				"FIELD3": "env3",
 				"FIELD5": "env5",
 			},
-			args: []string{"-field1", "arg1", "-field2=3", "--field6=required", "--field0"},
+			args: []string{"-dfield0=true", "-field1", "arg1", "-field2=3", "--field6=required", "--field0"},
 			wantConfig: TestConfig{
-				Field0: true,
-				Field1: "arg1",
-				Field2: 3,
-				Field3: "env3",
-				Field4: "file4",
-				Field5: "env5",
-				Field6: "required",
+				DField0: true,
+				Field0:  true,
+				Field1:  "arg1",
+				Field2:  3,
+				Field3:  "env3",
+				Field4:  "file4",
+				Field5:  "env5",
+				Field6:  "required",
 			},
 			wantErr: nil,
 		},
@@ -114,9 +123,17 @@ func TestLoad(t *testing.T) {
 			name:       "Required field not set",
 			file:       "",
 			env:        map[string]string{},
-			args:       []string{},
+			args:       []string{"-dfield0=true"},
 			wantConfig: TestConfig{},
-			wantErr:    errors.New("required parameter not set: Field6"),
+			wantErr:    fmt.Errorf("%s: %s", ErrRequiredNotSet.Error(), "Field6"),
+		},
+		{
+			name:       "Depends field not set",
+			file:       "",
+			env:        map[string]string{},
+			args:       []string{"-field6=required"},
+			wantConfig: TestConfig{},
+			wantErr:    fmt.Errorf("%s: %s", ErrDependsNotSet.Error(), "DField0"),
 		},
 	}
 
